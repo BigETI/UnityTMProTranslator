29cd7adb83d8447a30ff242ead81bcc49436d16b
BigETI <bigeti.ethem@gmail.com>
2019-09-03 22:16:31 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Unity Text Mesh Pro Translator
+
+## Description
+This package is an extension to the [Unity Translator package](https://github.com/BigETI/UnityTranslator) at https://github.com/BigETI/UnityTranslator.
+It contains a script to translate a game object with a Text Mesh Pro UGUI component.
+
+## License
+[The license](https://github.com/BigETI/UnityTMProTranslator/blob/master/LICENSE) for this package is available at https://github.com/BigETI/UnityTMProTranslator/blob/master/LICENSE
---
+++ Scripts/Controllers/TMProTranslatorTriggerScript.cs
@@ -0,0 +1,46 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityTranslator.Objects;
+
+/// <summary>
+/// Unity TMPro translator triggers namespace
+/// </summary>
+namespace UnityTMProTranslator.Triggers
+{
+    /// <summary>
+    /// TMPro Translator trigger script class
+    /// </summary>
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class TMProTranslatorTriggerScript : MonoBehaviour
+    {
+        /// <summary>
+        /// Translation object
+        /// </summary>
+        [SerializeField]
+        private TranslationObjectScript translationObject = default;
+
+        /// <summary>
+        /// Translation
+        /// </summary>
+        public string Translation
+        {
+            get
+            {
+                return ((translationObject == null) ? string.Empty : translationObject.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Start
+        /// </summary>
+        private void Start()
+        {
+            TextMeshProUGUI text = GetComponent<TextMeshProUGUI>();
+            if (text != null)
+            {
+                text.text = Translation;
+            }
+            Destroy(this);
+        }
+    }
+}
