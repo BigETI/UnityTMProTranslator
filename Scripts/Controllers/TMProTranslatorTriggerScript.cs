ea91c0b9becba4de3db34e609229526ae73af029
BigETI <bigeti.ethem@gmail.com>
2020-01-28 08:56:16 +0100

    Updated script according to dependency update.

--- Scripts/Controllers/TMProTranslatorTriggerScript.cs
+++ Scripts/Controllers/TMProTranslatorTriggerScript.cs
@@ -11,18 +11,18 @@ namespace UnityTMProTranslator.Triggers
     /// TMPro Translator trigger script class
     /// </summary>
     [RequireComponent(typeof(TextMeshProUGUI))]
-    public class TMProTranslatorTriggerScript : ATranslatorTriggerScript
+    public class TMProTranslatorTriggerScript : ATextTranslatorTriggerScript
     {
         /// <summary>
         /// Update text
         /// </summary>
         /// <param name="text">Text</param>
-        protected override void UpdateText(string text)
+        protected override void UpdateString(string text)
         {
             TextMeshProUGUI text_component = GetComponent<TextMeshProUGUI>();
             if (text_component != null)
             {
-                text_component.text = Translation;
+                text_component.text = StringTranslation;
             }
         }
     }
